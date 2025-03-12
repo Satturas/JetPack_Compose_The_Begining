e9d99942cc046c6afb713076bd2d62232f59c5f3
Peter <klintsov.p@mail.ru>
2025-03-12 11:03:15 +0300

    preview_boxes

--- app/src/main/java/com/example/jetpackcomposethebegining/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposethebegining/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.jetpackcomposethebegining
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -45,22 +56,54 @@ class MainActivity : ComponentActivity() {
 
     @Preview(name = "portrait", showSystemUi = true)
     @Composable
-    fun TextPreview2() {
-        Text(
-            text = stringResource(R.string.jetpack_text),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
+    fun ImagePreview() {
+        Image(
+            modifier = Modifier
+                .size(240.dp, 120.dp)
+                .background(Color.Black),
+            painter = painterResource(id = R.drawable.image),
+            contentScale = ContentScale.None,
+            alignment = Alignment.TopEnd,
+            contentDescription = stringResource(id = R.string.standard_icon)
         )
     }
 
     @Preview(name = "portrait", showSystemUi = true)
     @Composable
-    fun TextPreview3() {
-        Text(
-            modifier = Modifier
-                .size(width = 120.dp, height = 60.dp)
-                .background(Color.LightGray),
-            text = stringResource(R.string.jetpack_text),
+    fun ButtonPreview() {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { Log.d("ButtonPreview", "Кнопка нажата") },
+            content = { Text("Нажми меня", color = Color.Black) },
+            enabled = true,
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(2.dp, Color.Gray),
+            elevation = ButtonDefaults.elevation(defaultElevation = 8.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
         )
+    }
+
+    @Composable
+    fun RoundInitials(initials: String) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = null,
+                )
+                Text(initials)
+            }
+        }
+    }
+
+    @Preview(name = "portrait", showSystemUi = true)
+    @Composable
+    fun RoundInitialsPreview() {
+        RoundInitials("АБ")
     }
 }
---
+++ app/src/main/java/com/example/jetpackcomposethebegining/NewActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jetpackcomposethebegining
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class NewActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    @Composable
+    fun RoundInitials(initials: String) {
+        Box(
+            modifier = Modifier.padding(vertical = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = null,
+            )
+            Text(
+                text = initials
+            )
+        }
+    }
+
+    @Composable
+    fun BoxTask() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color.LightGray)
+        ) {
+            RoundInitials("АБ")
+        }
+    }
+
+    @Preview(name = "portrait", showSystemUi = true)
+    @Composable
+    fun Task1Preview() {
+        BoxTask()
+    }
+}
